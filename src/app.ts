911d21fd02e7faa905471d8a87bff987567e1859
Mendie Ekemini <ekemboy@gmail.com>
2023-07-06 22:10:03 +0800

    add required packages

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { OpenAI } from "langchain/llms/openai";
+
+import * as dotenv from "dotenv";
+dotenv.config()
+
+export const run = async () => {
+    const model = new OpenAI({temperature: 0.9});
+
+    const res = await model.call("What would be a good company name a company that makes colorful socks? ")
+
+    console.log({res});
+    
+};
+
+run();
